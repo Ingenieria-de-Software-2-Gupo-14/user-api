a00711c5072fbcb388b174024ef6049e426606a4
Martin Pata <martin.pata@mercadolibre.com>
2025-04-18 13:42:07 -0300

    feat: create auth middleware

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -9,10 +9,7 @@ import (
 
 // GenerateToken genera un token JWT para el usuario.
 func GenerateToken(userId int) (string, error) {
-	secret := os.Getenv("JWT_SECRET")
-	if secret == "" {
-		secret = "secret" // valor por defecto
-	}
+	secret := GetJWTSecret()
 
 	claims := jwt.MapClaims{
 		"user_id": userId,
@@ -21,4 +18,12 @@ func GenerateToken(userId int) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(secret))
+}
+
+func GetJWTSecret() string {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		secret = "secret" // valor por defecto
+	}
+	return secret
 }
