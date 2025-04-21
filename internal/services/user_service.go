32bcfbf5d50791929ca116b92bb3882fe768ea9a
bmorselrtto <bmorseletto@fi.uba.ar>
2025-04-21 18:21:54 -0300

    Fixed issue where modifyUser didnt hash the new password

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -64,6 +64,11 @@ func (s *userService) GetAllUsers(ctx context.Context) (users []models.User, err
 }
 
 func (s *userService) ModifyUser(ctx context.Context, user *models.User) error {
+	hashPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = hashPassword
 	return s.db.ModifyUser(ctx, user)
 
 }
