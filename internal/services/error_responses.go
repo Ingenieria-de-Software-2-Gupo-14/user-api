209b1ae7112df8d18c468d5579e3176061ee1083
bmorselrtto <bmorseletto@fi.uba.ar>
2025-04-24 13:55:03 -0300

    error responses test

--- internal/services/error_responses.go
+++ internal/services/error_responses.go
@@ -8,10 +8,6 @@ import (
 // CreateErrorResponse creates and returns an error response struct according to the status code, in case of not recognizing the status code and internal server error response is created
 func CreateErrorResponse(statusCode int, instance string) ErrorResponse {
 	switch statusCode {
-	case StatusMissingTitle:
-		return ErrorResponse{Type: ErrorTypeBlank, Title: ErrorTitleMissingTitle, Status: http.StatusBadRequest, Detail: ErrorDescriptionMissingTitle, Instance: instance}
-	case StatusMissingDescription:
-		return ErrorResponse{Type: ErrorTypeBlank, Title: ErrorTitleMissingDescription, Status: http.StatusBadRequest, Detail: ErrorDescriptionMissingDescription, Instance: instance}
 	case StatusUserNotFound:
 		return ErrorResponse{Type: ErrorTypeBlank, Title: ErrorTitleUserNotFound, Status: http.StatusNotFound, Detail: ErrorDescriptionUserNotFound, Instance: instance}
 	case http.StatusInternalServerError:
