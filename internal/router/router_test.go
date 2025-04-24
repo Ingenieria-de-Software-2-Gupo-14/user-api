f3d6cb9855e0495ef6c97e531c161a8784650878
bmorselrtto <bmorseletto@fi.uba.ar>
2025-04-24 13:55:11 -0300

    router tests

---
+++ internal/router/router_test.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"ing-soft-2-tp1/internal/repositories"
+	"testing"
+)
+
+func TestCreateRouter(t *testing.T) {
+	db, _, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	mockRepo := repositories.CreateDatabase(db)
+
+	result := CreateRouter(mockRepo)
+	assert.NotNil(t, result)
+}
