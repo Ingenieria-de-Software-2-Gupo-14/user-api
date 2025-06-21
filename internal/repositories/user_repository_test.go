7398b51017e631a1ee4b0f050147f4f32a3e78cf
bmorselrtto <bmorseletto@fi.uba.ar>
2025-06-21 16:24:15 -0300

    fix: user_repository.go tests done in user_repository_test.go

--- internal/repositories/user_repository_test.go
+++ internal/repositories/user_repository_test.go
@@ -3,13 +3,14 @@ package repositories
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 
 	"github.com/Ingenieria-de-Software-2-Gupo-14/user-api/internal/models"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateDatabase(t *testing.T) {
@@ -324,4 +325,224 @@ func TestDatabase_ModifyPassword(t *testing.T) {
 
 	err = database.ModifyPassword(ctx, 1, "test")
 	assert.NoError(t, err)
+}
+
+func TestUserRepository_AddNotificationToken(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+	userId := 1
+	token := "123456"
+
+	mock.ExpectQuery(`INSERT INTO notifications \(user_id, token\) VALUES \(\$1, \$2\)`).
+		WithArgs(userId, token).
+		WillReturnRows(sqlmock.NewRows([]string{}))
+
+	err = repo.AddNotificationToken(ctx, userId, token)
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_GetUserNotificationsToken(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+	userId := 1
+	now := time.Now()
+
+	mock.ExpectQuery(`SELECT token, created_time FROM notifications WHERE user_id = \$1`).
+		WithArgs(userId).
+		WillReturnRows(sqlmock.NewRows([]string{"token", "created_time"}).
+			AddRow("token1", now).
+			AddRow("token2", now))
+
+	result, err := repo.GetUserNotificationsToken(ctx, userId)
+	assert.NoError(t, err)
+	assert.Len(t, result.NotificationTokens, 2)
+	assert.Equal(t, "token1", result.NotificationTokens[0].NotificationToken)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_GetUserNotificationsToken_NotFound(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+
+	rows := sqlmock.NewRows([]string{"token", "created_time"}).
+		AddRow("tok", time.Now())
+	rows.RowError(0, ErrNotFound)
+
+	mock.ExpectQuery(`SELECT token, created_time FROM notifications WHERE user_id = \$1`).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	_, err = repo.GetUserNotificationsToken(ctx, 1)
+	assert.Error(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_SetVerifiedTrue(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+	userID := 1
+
+	mock.ExpectExec(`UPDATE users SET verified = true where id = \$1`).
+		WithArgs(userID).
+		WillReturnResult(sqlmock.NewResult(0, 1)) // 1 row affected
+
+	err = repo.SetVerifiedTrue(ctx, userID)
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_AddPasswordResetToken(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+
+	userID := 1
+	email := "user@example.com"
+	token := "123456"
+	expiration := time.Now().Add(1 * time.Hour)
+
+	mock.ExpectExec(`INSERT INTO password_reset \(user_id, email, token, token_expiration\) VALUES \(\$1, \$2, \$3, \$4\)`).
+		WithArgs(userID, email, token, expiration).
+		WillReturnResult(sqlmock.NewResult(1, 1)) // simulate 1 row inserted
+
+	err = repo.AddPasswordResetToken(ctx, userID, email, token, expiration)
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_GetPasswordResetTokenInfo(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+
+	token := "reset-token"
+	expTime := time.Now()
+	mail := "user@example.com"
+	userId := 1
+
+	mock.ExpectQuery(`SELECT email, user_id, token_expiration, used FROM password_reset WHERE token = \$1`).
+		WithArgs(token).
+		WillReturnRows(sqlmock.NewRows([]string{"email", "user_id", "token_expiration", "used"}).
+			AddRow("user@example.com", userId, expTime, false))
+
+	result, err := repo.GetPasswordResetTokenInfo(ctx, token)
+	assert.NoError(t, err)
+	assert.Equal(t, mail, result.Email)
+	assert.Equal(t, userId, result.UserId)
+	assert.Equal(t, expTime, result.Exp)
+	assert.False(t, result.Used)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_SetPasswordTokenUsed(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+	token := "123456"
+
+	mock.ExpectExec(`UPDATE password_reset SET used = true where token = \$1`).
+		WithArgs(token).
+		WillReturnResult(sqlmock.NewResult(0, 1)) // simulate success
+
+	err = repo.SetPasswordTokenUsed(ctx, token)
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_SetNotificationPreference(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+
+	preference := models.NotificationPreferenceRequest{
+		NotificationType:       "email_notifications",
+		NotificationPreference: true,
+	}
+	userID := 1
+
+	expectedQuery := fmt.Sprintf("UPDATE users SET %s = \\$2 WHERE id = \\$1", preference.NotificationType)
+
+	mock.ExpectExec(expectedQuery).
+		WithArgs(userID, preference.NotificationPreference).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err = repo.SetNotificationPreference(ctx, userID, preference)
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+func TestUserRepository_CheckPreference(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+	userID := 1
+	notificationType := "exam_notifications"
+
+	query := fmt.Sprintf("SELECT %s FROM users WHERE id = \\$1", notificationType)
+	mock.ExpectQuery(query).
+		WithArgs(userID).
+		WillReturnRows(sqlmock.NewRows([]string{notificationType}).AddRow(true))
+
+	result, err := repo.CheckPreference(ctx, userID, notificationType)
+	assert.NoError(t, err)
+	assert.True(t, result)
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestUserRepository_GetNotificationPreference(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	repo := CreateUserRepo(db)
+	ctx := context.Background()
+	userID := 1
+	examNotification := true
+	homeworkNotification := false
+	socialNotification := true
+
+	mock.ExpectQuery(`SELECT exam_notification, homework_notification, social_notification FROM users WHERE id = \$1`).
+		WithArgs(userID).
+		WillReturnRows(sqlmock.NewRows([]string{
+			"exam_notification", "homework_notification", "social_notification",
+		}).AddRow(examNotification, homeworkNotification, socialNotification))
+
+	result, err := repo.GetNotificationPreference(ctx, userID)
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, examNotification, result.ExamNotification)
+	assert.Equal(t, homeworkNotification, result.HomeworkNotification)
+	assert.Equal(t, socialNotification, result.SocialNotification)
+	assert.NoError(t, mock.ExpectationsWereMet())
 }
